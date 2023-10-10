e69707debf924e013e0a115dc9c05a742995ed0b
mwangiii <mwangi8kevin@gmail.com>
2023-10-10 11:44:54 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+def pascal_triangle(n):
+    p = [[1]]
+
+    if n <= 0:
+        return []
+
+    for i in range(1, n+1):
+        row = [1]
+        for j in range(1, i):
+            if j == i - 1:
+                row.append(1)
+            else:
+                row.append(p[i - 1][j] + p[i - 1][j - 1])
+        p.append(row)
+
+    return p
+
+            
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,3 @@
+# Pascal's triangle
+## Description
+This is a program that prints the first n rows of Pascal's triangle.

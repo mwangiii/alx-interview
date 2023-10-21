fa01c2be6811cdc4dd8d43cb871dd367f14eb77d
mwangiii <mwangi8kevin@gmail.com>
2023-10-21 14:16:57 +0300

    readme file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  Lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """ a method that determines if all the boxes can be opened """
+    boxes = [[]]
+    for n in range(n):
+        for b in range (0 , n-1):
+            if b = n:
+                return True
+            else:
+                return False

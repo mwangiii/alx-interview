98db3d5bc41146256dbdb6fe3f49ea3dd21d9560
mwangiii <mwangi8kevin@gmail.com>
2023-11-13 13:01:49 +0300

    starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,50 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+const apiKey = ' https://swapi-api.alx-tools.com/api/'
+
+// The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi” 
+const movieId = process.argv[2]
+
+
+const request = require('request');
+
+if (!movieId) {
+  console.error('Please provide a movie ID as a command line argument.');
+  process.exit(1);
+}
+
+const apiUrl = `apikey ${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Unexpected status code: ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  if (!characters || characters.length === 0) {
+    console.log('No characters found for this movie.');
+    process.exit(0);
+  }
+
+  // Displaying one character name per line
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (!charError && charResponse.statusCode === 200) {
+        const characterData = JSON.parse(charBody);
+        console.log(characterData.name);
+      } else {
+        console.error('Error fetching character:', charError);
+      }
+    });
+  });
+});
